99042ad97fbd1c2dcfc51915d485023d59ba8f56
Tania-crma <123047824@upq.edu.mx>
2025-01-23 22:30:13 -0600

    Introducción a Java

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -1,9 +1,75 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//zona de importacion
+import java.util.Scanner;
+//Clase principal
 public class Main {
+    //Metodo Main
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello Tania Asuncion Cruz Marquez");
+        System.out.println("Hola mundo, soy Tania");
+        /*Comentario
+        Comentario
+        comentario*/
+
+        //2. Cadenas
+        String cadena= "Tania" + "Cruz" + "Marquez";
+        System.out.println(cadena);
+        System.out.println(cadena.length());
+
+        //Extraccion de caracteres en base a parametros
+
+        System.out.println(cadena.substring(2,5));
+        System.out.println(cadena.substring(2));
+        System.out.println(cadena.substring(0,5));
+
+
+        //3. variables
+        String cadena = "333", cadena2;
+        int entero = 3,entero2,entero3;
+        double decimal=3.3, decimal2,decimal3;
+
+        //convertir cadena a int
+        entero2=Integer.parseInt(cadena);
+        System.out.println(entero2);
+        //Convertir double a int
+        entero3=(int) decimal;
+        System.out.println(entero3);
+
+        //convertir double a cadena
+
+        cadena2= String.valueOf(decimal);
+        System.out.println(cadena2);
+
+        //convertir int a double
+        decimal2=Double.valueOf(entero);
+        System.out.println(decimal2);
+
+        //4. solicitud de datos
+        Scanner scn= new Scanner(System.in);
+        System.out.println("Escriba una cadena");
+        String cadena= scn.nextLine();
+
+        System.out.println("Escribe un numero entero");
+        int entero= scn.nextInt();
+
+        System.out.println("Escribe un numero decimal");
+        double decimal= scn.nextDouble();
+
+        //5. boolean, operadores logicos y de comparacion
+        System.out.println(10 > 9);
+        System.out.println(10 == 9);
+        System.out.println(10 < 9);
+        System.out.println(10 >= 9);
+        System.out.println(10 <= 9);
+        System.out.println(10 != 9);
+
+        //Operadores logicos
+        int x= 3;
+        System.out.println(x<5 && x>10);
+        System.out.println(x<5 || x>10);
+        System.out.println(!(x<5 && x>10));
+        System.out.println(!(x<5 || x>10));
+
+
     }
 }
