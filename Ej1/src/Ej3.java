45233f2a760f6950fdfa1bbba5efd6d295979ff0
Tania-crma <123047824@upq.edu.mx>
2025-01-28 15:42:07 -0600

    Ejercicio 4 Cantidad de letras

--- Ej1/src/Ej3.java
+++ Ej1/src/Ej3.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class Ej3 {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingresa tu nombre: ");
+        String nombre = entrada.nextLine();
+
+            System.out.println("Cantidad de letras: " + nombre.length());
+    }
 }
