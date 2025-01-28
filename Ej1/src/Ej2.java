a7b21b8ede5dae521bb0a68263fff4eef9e0f7d4
Tania-crma <123047824@upq.edu.mx>
2025-01-28 15:31:45 -0600

    Ejercicio 2 Nombre tres veces

--- Ej1/src/Ej2.java
+++ Ej1/src/Ej2.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Ej2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingresa tu nombre: ");
+        String nombre = entrada.nextLine().trim();
+
+        String mayusculas = nombre.toUpperCase();
+        String minusculas = nombre.toLowerCase();
+
+        String inicial = nombre.isEmpty() ? "" :
+                nombre.substring(0, 1).toUpperCase() + nombre.substring(1).toLowerCase();
+
+        System.out.println("Nombre en mayusculas: " + mayusculas);
+        System.out.println("Nombre en minusculas: " + minusculas);
+        System.out.println("Solo inicial: " + inicial);
+    }
 }
+
---
+++ Ej1/src/Ej3.java
@@ -0,0 +1,3 @@
+public class Ej3 {
+    public static void main(String[] args) {}
+}
