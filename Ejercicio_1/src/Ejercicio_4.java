ed83809dd7c85e00f6624f25a8e89a3c09d4b40a
Tania-crma <123047824@upq.edu.mx>
2025-01-30 15:28:15 -0600

    Ejercicio 4 Es un palidromo?

--- Ejercicio_1/src/Ejercicio_4.java
+++ Ejercicio_1/src/Ejercicio_4.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class Ejercicio_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese una palabra o frase: ");
+        String entrada = sc.nextLine().toLowerCase();
+
+        String textoLimpio = entrada.replaceAll("[^a-zA-Z0-9]", "");
+
+        String textoInvertido = invertir(textoLimpio);
+
+        if (textoLimpio.equals(textoInvertido)) {
+            System.out.println("Es un palíndromo.");
+        } else {
+            System.out.println("No es un palíndromo.");
+        }
+
+        sc.close();
+    }
+
+    public static String invertir(String texto) {
+        String textoInvertido = "";
+
+        for (int i = texto.length() - 1; i >= 0; i--) {
+            textoInvertido += texto.charAt(i);
+        }
+
+        return textoInvertido;
+    }
 }
+
