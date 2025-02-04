2efd8ce6ac6fb03194bdce7023edc2ed721e5683
Tania-crma <123047824@upq.edu.mx>
2025-02-04 16:39:32 -0600

    Examen

--- Examen 1er parcial/src/Ejercicio_2.java
+++ Examen 1er parcial/src/Ejercicio_2.java
@@ -1,7 +1,7 @@
 public class Ejercicio_2 {
     public static void main(String[] args) {
 
-        for (int i = 10; i < 1001; i+= 10) {
+        for (int i = 10; i < 1001; i += 10) {
             System.out.print(i + " ");
         }
     }
